e8b6f0397c03dcd8cf9767b465228541f800c8e7
Tanmoy07tech <Tanmoy07tech@users.noreply.github.com>
2021-06-19 21:08:56 +0530

    Initial Phase of scrapping_git_repository

---
+++ login.py
@@ -0,0 +1,15 @@
+n=int(input("Enter the number how many numbers you want to input"))
+
+all_names=[]
+
+for i in range(n):
+    name=input()
+    all_names.append(name)
+
+
+for i in len(all_name):
+    if all_name[i]
+
+
+
+    
